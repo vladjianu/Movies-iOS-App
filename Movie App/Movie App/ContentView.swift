c06e9a052fe6f94cca69340ff68c9a5df838e298
vladjianu <vladjianu@192.168.1.19>
2024-12-24 16:00:50 +0200

    project setup

---
+++ Movie App/Movie App/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Movie App
+//
+//  Created by Vlad Jianu on 24/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie App/Movie App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie App
+//
+//  Created by Vlad Jianu on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
