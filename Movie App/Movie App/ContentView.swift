6a107de68ecdea7ef036bdab4599f27378da415b
vladjianu <vladjianu@192.168.1.19>
2024-12-24 16:16:31 +0200

    tabview with 4 tabs created

--- Movie App/Movie App/ContentView.swift
+++ Movie App/Movie App/ContentView.swift
@@ -9,6 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        TabView{
+            Tab("Home", systemImage: "house") {
+                Text("Home")
+            }
+            Tab("Upcoming", systemImage: "play.circle") {
+                Text("Upcoming")
+            }
+            Tab("Search", systemImage: "magnifyingglass") {
+                Text("Search")
+            }
+            Tab("Download", systemImage: "arrow.down.to.line") {
+                Text("Download")
+            }
+        }
         
     }
 }
