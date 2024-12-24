c35743dec2ebe53073046a5fc10c9d223d71388b
vladjianu <vladjianu@192.168.1.19>
2024-12-24 17:04:08 +0200

    Refactored hard-coded strings and icon names into a Constants file for better maintainability.

---
+++ Movie App/Movie App/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Movie App
+//
+//  Created by Vlad Jianu on 24/12/2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
+}
--- Movie App/Movie App/ContentView.swift
+++ Movie App/Movie App/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView{
-            Tab("Home", systemImage: "house") {
-                Text("Home")
+            Tab(Constants.homeString, systemImage: Constants.homeIconString) {
+                Text(Constants.homeString)
             }
-            Tab("Upcoming", systemImage: "play.circle") {
-                Text("Upcoming")
+            Tab(Constants.upcomingString, systemImage: Constants.upcomingIconString) {
+                Text(Constants.upcomingString)
             }
-            Tab("Search", systemImage: "magnifyingglass") {
-                Text("Search")
+            Tab(Constants.searchString, systemImage: Constants.searchIconString) {
+                Text(Constants.searchString)
             }
-            Tab("Download", systemImage: "arrow.down.to.line") {
-                Text("Download")
+            Tab(Constants.downloadString, systemImage: Constants.downloadIconString) {
+                Text(Constants.downloadString)
             }
         }
         
